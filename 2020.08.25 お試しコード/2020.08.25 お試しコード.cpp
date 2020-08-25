66ac9ecadf4ddb23f1b718aeb0f54381e0fb5ea0
Yumi Nose <yumi.nose28@gmail.com>
2020-08-25 14:36:16 +0900

    プロジェクト ファイルを追加します。

---
+++ 2020.08.25 お試しコード/2020.08.25 お試しコード.cpp
@@ -0,0 +1,28 @@
+﻿// 2020.08.25 お試しコード.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i < 101; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0) {
+            cout << "fizzbuzz" << endl;
+        }
+
+        else if (i % 3 == 0) {
+            cout << "fizz" << endl;
+        }
+        else if (i % 5 == 0)
+        {
+            cout << "Buzz" << endl;
+        }
+        else
+        {
+            cout << i << endl;
+        }
+    }
+}
